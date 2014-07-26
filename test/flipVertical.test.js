71e19db7e70059723afacd358935076ee45c7924
fentas <jan.guth@gmail.com>
2014-07-26 03:23:12 +0200

    test case: *.flipVertical()

---
+++ test/flipVertical.test.js
@@ -0,0 +1,10 @@
+
+QUnit.test( "*.flipVertical()", function( assert ) {
+  var canvas = assert.data.ctx.canvas;
+  assert.pixelEqual(canvas, 170, 200, 235, 235, 235, 255, 'before: pixel top.');
+  assert.pixelEqual(canvas, 170, 450, 228,  77,  38, 255, 'before: pixel bottom.');
+
+  assert.data.ctx.filter.flipVertical();
+  assert.pixelEqual(canvas, 170, 200, 235, 235, 235, 255, 'after: pixel top.');
+  assert.pixelEqual(canvas, 170, 450,   0,   0,   0, 255, 'after: pixel bottom.');
+});
